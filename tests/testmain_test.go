a742aebbf0bd0f568efffc4f378b76d9395a7902
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2021-06-22 18:34:19 -0700

    Add a waiter in the integration test code as well for local machine executions.

--- tests/testmain_test.go
+++ tests/testmain_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -44,12 +45,34 @@ type emailUserSignupRequest struct {
 }
 
 func TestMain(m *testing.M) {
+	waitForPulumiAPIReadiness()
+
 	if err := createPulumiEmailUser(); err != nil {
 		panic(fmt.Sprintf("Error creating email-based user: %v", err))
 	}
 
 	exitCode := m.Run()
 	os.Exit(exitCode)
+}
+
+func waitForPulumiAPIReadiness() {
+	timeout := false
+	time.AfterFunc(120*time.Second, func() {
+		timeout = true
+	})
+	for {
+		resp, err := http.Get(fmt.Sprintf("%s/api/status", pulumiAPIURI))
+		if err != nil {
+			panic("Failed calling the API's status endpoint")
+		}
+
+		if resp.StatusCode == http.StatusOK {
+			break
+		}
+		if timeout {
+			panic("Timed out waiting for the API's status endpoint to become ready")
+		}
+	}
 }
 
 type loginWithGitHubResponse struct {
