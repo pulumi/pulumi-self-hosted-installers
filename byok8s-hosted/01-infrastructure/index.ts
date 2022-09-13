6b166bc4be89fd0a30a76f8e28b97e722172f557
MitchellGerdisch <mitch@pulumi.com>
2022-09-13 06:54:30 -0500

    fix some stack outputs

--- byok8s-hosted/01-infrastructure/index.ts
+++ byok8s-hosted/01-infrastructure/index.ts
@@ -1,11 +1,13 @@
 import { config } from "./config";
 
-export const storageAccessKey = config.storageAccessKey;
-export const storageSecretKey = config.storageSecretKey;
+export const serviceAccountAccessKeyId = config.storageAccessKey;
+export const storageServiceAccountSecretAccessKey = config.storageSecretKey;
 export const storageCheckpointBucket = config.storageCheckpointBucket;
 export const storagePolicyPackBucket = config.storagePolicyPackBucket;
 export const dbHost = config.dbHost;
 export const dbPort = config.dbPort;
+export const dbConnectionString = `${dbHost}:${dbPort}`;
+export const dbServerName = dbHost;
 export const dbUsername = config.dbUsername;
 export const dbUserPassword = config.dbUserPassword;
 export const stackName1 = config.stackName;
