6804bf91370cf9e65a7f92873baa67f3a75b97c7
Richard Shade <richard@pulumi.com>
2024-10-22 10:07:36 -0500

    Adding in example of returning envVars, fixes: #156

--- components-microstacks/index.ts
+++ components-microstacks/index.ts
@@ -1,1 +1,12 @@
 import { OpenSearch } from "./openSearch"
+import { EnvVar } from "./types"
+
+const consoleEnvVars: EnvVar[] = []
+const apiEnvVars: EnvVar[] = []
+
+
+// const os = new OpenSearch("opensearc", {
+//     namespace: 
+// }, {})
+
+// apiEnvVars.concat(os.envVars)
