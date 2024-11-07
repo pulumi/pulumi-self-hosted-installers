d4b0e656134784135dedcf6e80a0397aa1609909
Richard Shade <richard@pulumi.com>
2024-11-07 14:24:39 -0600

    Adding in config values

--- tests/aws_eks_ts_test.go
+++ tests/aws_eks_ts_test.go
@@ -1,6 +1,8 @@
 package tests
 
 import (
+	"os"
+
 	"github.com/pulumi/providertest/pulumitest"
 
 	"path/filepath"
@@ -10,21 +12,23 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/auto/optpreview"
 )
 
-func runCycle(t *testing.T, basePath string, folder string) *pulumitest.PulumiTest {
-	p := pulumitest.NewPulumiTest(t, filepath.Join(basePath, "01-iam"), opttest.StackName("prod"), opttest.SkipInstall())
-	p.UpdateSource(t, filepath.Join(basePath, folder))
+func runCycle(t *testing.T, basePath string, folder string, additionalConfig map[string]string) *pulumitest.PulumiTest {
+	os.MkdirAll("/tmp/.pulumi", os.FileMode(0777))
+	os.Setenv("PULUMI_BACKEND_URL", "file:///tmp/.pulumi")
+	p := pulumitest.NewPulumiTest(t, filepath.Join(basePath, folder), opttest.StackName("prod"), opttest.SkipInstall(), opttest.UseAmbientBackend())
 	CopyFile(filepath.Join(p.WorkingDir(), "Pulumi.README.yaml"), filepath.Join(p.WorkingDir(), "Pulumi.prod.yaml"))
-	p.SetConfig(t, "aws:region", "us-east-1")
+	p.SetConfig(t, "aws:region", "us-west-2")
+	if additionalConfig != nil {
+		for key, value := range additionalConfig {
+			p.SetConfig(t, key, value)
+		}
+	}
 	p.Install(t)
 	p.Up(t)
 	p.Preview(t, optpreview.ExpectNoChanges())
 	return p
 }
 func TestAwsEksTsExamples(t *testing.T) {
-	// "01-iam"
-	// "02-networking",
-	// "05-eks-cluster",
-	// "10-cluster-svcs",
 	// "15-state-policies-mgmt",
 	// "20-database",
 	// "25-insights",
@@ -35,15 +39,14 @@ func TestAwsEksTsExamples(t *testing.T) {
 	t.Run("TestAwsEksTs", func(t *testing.T) {
 		checkAwsEnvVars(t)
 		basePath := "../eks-hosted"
-		iam := runCycle(t, basePath, "01-iam")
-		networking := runCycle(t, basePath, "02-networking")
-		// p01 := pulumitest.NewPulumiTest(t, filepath.Join(basePath, "01-iam"), opttest.StackName("prod"), opttest.SkipInstall())
-		// CopyFile(filepath.Join(p01.WorkingDir(), "Pulumi.README.yaml"), filepath.Join(p01.WorkingDir(), "Pulumi.prod.yaml"))
-		// p01.SetConfig(t, "aws:region", "us-east-1")
-		// p01.Install(t)
-		// p01.Up(t)
-		// p01.Preview(t, optpreview.ExpectNoChanges())
-		defer networking.Destroy(t)
+		var emptyConfig map[string]string
+		iam := runCycle(t, basePath, "01-iam", emptyConfig)
 		defer iam.Destroy(t)
+		networking := runCycle(t, basePath, "02-networking", emptyConfig)
+		defer networking.Destroy(t)
+		eksCluster := runCycle(t, basePath, "05-eks-cluster", emptyConfig)
+		defer eksCluster.Destroy(t)
+		clusterSvcs := runCycle(t, basePath, "10-cluster-svcs", emptyConfig)
+		defer clusterSvcs.Destroy(t)
 	})
 }
