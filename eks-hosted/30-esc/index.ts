73ad1b47a753f3f2a62ab5847537c372a1567596
MitchellGerdisch <mitch@pulumi.com>
2024-12-04 13:36:05 -0600

    add and clean up byo infra options

--- eks-hosted/30-esc/index.ts
+++ eks-hosted/30-esc/index.ts
@@ -7,6 +7,13 @@ const baseName = config.baseName
 ///////////////
 // Resources for ESC feature - which at this point is very minimal, but kept in a separate stack in case future features require more infrastructure..
 
-// Create S3 Bucket for the service checkpoints and policy packs.
-const escBucket = new aws.s3.Bucket(`${baseName}-esc`, {}, { protect: true});
-export const escBucketName = escBucket.id;
+// Create S3 Bucket for the ESC storage - if not using an existing bucket.
+let escBucketName: pulumi.Output<string> | string
+if (!config.escBucketName) {
+    const escBucket = new aws.s3.Bucket(`${baseName}-esc`, {}, { protect: true});
+    escBucketName = escBucket.id;
+} else {
+    escBucketName = config.escBucketName;
+}
+
+export { escBucketName };
