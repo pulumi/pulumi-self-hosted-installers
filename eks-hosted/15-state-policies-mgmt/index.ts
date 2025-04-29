fb7a9c2b2a59d6466143e4785596528abacdfbe1
Davide Massarenti <davide@pulumi.com>
2025-04-29 12:04:21 -0700

    Update infrastructure to expose new Engine Events and Checkpoints V2 buckets

--- eks-hosted/15-state-policies-mgmt/index.ts
+++ eks-hosted/15-state-policies-mgmt/index.ts
@@ -4,9 +4,11 @@ import { config } from "./config";
 
 const baseName = config.baseName 
 
-export let checkpointsS3BucketName; 
-export let policyPacksS3BucketName; 
-export let eventsS3BucketName; 
+export let checkpointsS3BucketName;
+export let checkpointsS3BucketNameV2;
+export let policyPacksS3BucketName;
+export let eventsS3BucketName;
+export let eventsS3BucketNameV2;
 
 ///////////////
 // Use provided S3 bucket or create S3 Buckets as applicable.
@@ -15,6 +17,13 @@ if (config.checkpointsS3BucketName) {
 } else {
   const checkpointsBucket = new aws.s3.Bucket(`${baseName}-checkpoints`, {}, { protect: true})
   checkpointsS3BucketName = checkpointsBucket.bucket;
+}
+
+if (config.checkpointsS3BucketNameV2) {
+  checkpointsS3BucketNameV2 = config.checkpointsS3BucketNameV2;
+} else {
+  const checkpointsBucket = new aws.s3.Bucket(`${baseName}-checkpoints-v2`, {}, { protect: true})
+  checkpointsS3BucketNameV2 = checkpointsBucket.bucket;
 } 
 
 if (config.policyPacksS3BucketName) {
@@ -29,4 +38,11 @@ if (config.eventsS3BucketName) {
 } else {
   const eventsBucket = new aws.s3.Bucket(`${baseName}-events`, {}, { protect: true});
   eventsS3BucketName = eventsBucket.bucket;
+}
+
+if (config.eventsS3BucketNameV2) {
+  eventsS3BucketNameV2 = config.eventsS3BucketNameV2;
+} else {
+  const eventsBucket = new aws.s3.Bucket(`${baseName}-events-v2`, {}, { protect: true});
+  eventsS3BucketNameV2 = eventsBucket.bucket;
 }
